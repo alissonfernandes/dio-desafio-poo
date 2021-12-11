af893710c0ac27f11100b6a7cabcb78f06fb7288
alissonfernandes <alissonfernandes200@gmail.com>
2021-12-11 17:02:53 -0300

    Create class 'Dev'

---
+++ src/main/java/br/com/desafio/dio/dominio/Dev.java
@@ -0,0 +1,62 @@
+package br.com.desafio.dio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudosInscritos;
+    private Set<Conteudo> conteudosConcluidos;
+
+    public Dev() {
+        this.conteudosInscritos = new LinkedHashSet<>();
+        this.conteudosConcluidos = new LinkedHashSet<>();
+    }
+
+    public void inscreverNoBootcamp(Boocamp boocamp) {
+
+    }
+
+    public void progredir(){}
+
+    public void calcXP(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+}
